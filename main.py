f99c5ccde00cd244044a31ae4a38b4835d935f5f
Sabrina-cpu <sabri12790@gmail.com>
2025-11-26 14:19:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -460,4 +460,5 @@ def get_anime_characters(mal_id: int):
 
 if __name__ == '__main__':
     # El host="0.0.0.0" es OBLIGATORIO para Render
-    app.run(host="0.0.0.0", port=5000
+    app.run(host="0.0.0.0", port=5000)
+
